f239ffb3496ae72d4354819ce9429f65a5bca981
Kenji Shimizu <jrywm121@gmail.com>
2019-03-17 22:59:12 +0900

    change to implemant dependency injection

--- interfaces/router/router.go
+++ interfaces/router/router.go
@@ -3,11 +3,12 @@ package router
 import (
 	"fmt"
 	"net/http"
+	"sealion/interfaces/handler"
 	"sealion/registry"
 )
 
 func Run(port int) error {
-	r := registry.NewRegistry()
-	http.Handle("/api/tasks", r.NewAppHandler())
+	h, _ := registry.Store.Get("AppHandler").(handler.TaskHandler)
+	http.Handle("/api/tasks", h)
 	return http.ListenAndServe(fmt.Sprintf(":%d", port), nil)
 }

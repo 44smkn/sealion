148355850c11346d0df1dd62f68d892e11211111
Kenji Shimizu <jrywm121@gmail.com>
2019-04-19 23:57:10 +0900

    add proecss that handle delete task

--- interfaces/router/router.go
+++ interfaces/router/router.go
@@ -10,5 +10,6 @@ import (
 func Run(port int) error {
 	h, _ := registry.Store.Get("AppHandler").(handler.TaskHandler)
 	http.Handle("/api/tasks", h)
+	http.Handle("/api/tasks/", h)
 	return http.ListenAndServe(fmt.Sprintf(":%d", port), nil)
 }

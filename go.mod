2fd5c626dd53e05695b522e0bc3581854b077401
Kenji Shimizu <jrywm121@gmail.com>
2019-06-01 23:03:41 +0900

    add logger pacakge "logrus"

--- go.mod
+++ go.mod
@@ -6,4 +6,5 @@ require (
 	github.com/gorilla/mux v1.7.2
 	github.com/kelseyhightower/envconfig v1.3.0
 	github.com/pkg/errors v0.8.1
+	github.com/sirupsen/logrus v1.4.2
 )
--- main.go
+++ main.go
@@ -4,10 +4,15 @@ import (
 	"log"
 	"flag"
 	"sealion/interfaces/router"
+	"github.com/sirupsen/logrus"
 )
 
 func main() {
 	port := flag.Int("port", 8080, "port to listen http request")
+	debugEnabled := flag.Bool("debug", false, "enable debug output in logs")
+	if *debugEnabled {
+		logrus.SetLevel(logrus.DebugLevel)
+	}
 	if err := router.Run(*port); err != nil {
 		log.Fatal("ListenAndServe:", err)
 	}
